0423a02e036d19414e5aea61c14f0f1b1ec5a7a7
Markus Rudy <webmaster@burgerdev.de>
2019-09-29 11:17:42 +0200

    use x86 instead of arm as the example platform

--- examples/helper-kubectl.sh
+++ examples/helper-kubectl.sh
@@ -8,5 +8,5 @@ echo "Usage: $0 { version }" >&2
 exit 1
 fi
 
-curl -sSL https://dl.k8s.io/v$1/kubernetes-client-linux-arm64.tar.gz |\
+curl -sSL https://dl.k8s.io/v$1/kubernetes-client-linux-amd64.tar.gz |\
     tar -xz kubernetes/client/bin/kubectl --to-stdout
